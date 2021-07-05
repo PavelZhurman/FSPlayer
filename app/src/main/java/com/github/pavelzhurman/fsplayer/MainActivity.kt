97d9354d80226f73055e00b7ef36128fff0e6136
GRODNO\Pavel_Zhurman <pavel_zhurman@epam.com>
2021-07-05 19:51:08 +0300

    Create interface Stubs. Create ImageLoader module. ViewBinding implemented in MiniPlayerView.

--- app/src/main/java/com/github/pavelzhurman/fsplayer/MainActivity.kt
+++ app/src/main/java/com/github/pavelzhurman/fsplayer/MainActivity.kt
@@ -2,30 +2,30 @@ package com.github.pavelzhurman.fsplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import com.bumptech.glide.Glide
-import com.github.pavelzhurman.core.Logger
-import com.github.pavelzhurman.ui.player.PlayerView
+import com.github.pavelzhurman.core.Stubs
+import com.github.pavelzhurman.fsplayer.databinding.ActivityMainBinding
+import com.github.pavelzhurman.image_loader.ImageLoader
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        Logger().logcatD("TagTag", localClassName)
-        Logger().logcatE("TagTag", localClassName)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val tvHelloWorld = findViewById<TextView>(R.id.tvHelloWorld)
-        tvHelloWorld.text = getString(R.string.hello_world)
+        val url = Stubs.Images().FAKE_POSTER_FREESOUND_SOUND
+        val url2 = Stubs.Images().FAKE_POSTER_TREE
+        val url3 = Stubs.Images().FAKE_POSTER_NYAN_CAT
 
-        val playerView: PlayerView = findViewById(R.id.player_view)
-        playerView.setArtist("Hello")
-        playerView.setSongName("It's me")
+        val miniPlayerBinding = binding.miniPlayerView.getBinding()
 
-        val url = "https://freesound.org/data/displays/2/2223_3525_wave_L.png"
-        Glide.with(this)
-            .load(url)
-            .into(playerView.getImageAlbum())
+        miniPlayerBinding?.apply {
+            textViewArtist.text = Stubs.Texts().ARTIST_EMINEM
+            textViewSongName.text = Stubs.Texts().SONG_LOSE_YOURSELF
+
+            ImageLoader().loadPoster(this@MainActivity, url3, imageAlbum)
+        }
+        miniPlayerBinding?.seekBar?.progress = 15
     }
 }
---
+++ core/src/main/java/com/github/pavelzhurman/core/Stubs.kt
@@ -0,0 +1,19 @@
+package com.github.pavelzhurman.core
+
+interface Stubs {
+
+    class Images {
+        val FAKE_POSTER_NYAN_CAT =
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdofMREX8LZvjiWjDLa_ZuGhQ4MzuE0elFFKTmSQd3y8Fg8ZdNLqYHZ6_0wZ9QDUWe0C8&usqp=CAU"
+        val FAKE_POSTER_TREE =
+            "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
+        val FAKE_POSTER_FREESOUND_SOUND =
+            "https://freesound.org/data/displays/2/2223_3525_wave_L.png"
+    }
+
+    class Texts {
+        val ARTIST_EMINEM = "Eminem"
+        val SONG_LOSE_YOURSELF = "Lose yourself"
+    }
+
+}
